12d6a9b31eb035df94edf43e5d93d3d0929486e4
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-10-26 22:34:50 -0500

    Expand API class

--- src/TerraformApi.ts
+++ src/TerraformApi.ts
@@ -14,15 +14,17 @@ var urls = require("../../aws-urls.json");
 console.log(urls)
 
 const HOME_DIR = require('os').homedir();
-const TOKEN_REGEX = /^\s*token = "([a-zA-Z0-9.]+)"$/
+const TOKEN_REGEX = /^\s*token = "([a-zA-Z0-9.]+)"$/;
+const REGISTRY_URL = 'https://registry.terraform.io';
+const REGISTRY_MODULES_PATH = '/v1/modules/'
 
 export class TerraformApi {
     public apiTokenExists (): boolean {
         console.log(`apiTokenExists called`);
         console.log(workspace.getConfiguration('terraform').get('terraformrc_file_path'));
 
-        const tfrcFile = HOME_DIR + '/.terraformrc'
-        console.log(tfrcFile)
+        const tfrcFile = HOME_DIR + '/.terraformrc';
+        console.log(tfrcFile);
 
         if (fs.existsSync(tfrcFile)) {
             let tfrcContents = fs.readFileSync(tfrcFile, 'utf8');
@@ -30,11 +32,56 @@ export class TerraformApi {
                 if (TOKEN_REGEX.test(line)) {
                     console.log("Successfully found token using regex: ");
                     const token = TOKEN_REGEX.exec(line)[1];
-                    console.log(token)
+                    console.log(token);
                 }
             }
         }
 
         return false;
+    }
+
+    private makeModuleRequest(path: string) {
+        console.log("Making request..")
+        
+        let url = REGISTRY_URL + REGISTRY_MODULES_PATH
+        
+        request(url, { json: true }, (err, res, body) => {
+            if (err) { console.log(err); }
+            console.log("Got request! Logging body..")
+            console.log(body);
+            let response = body;
+        });
+    }
+
+    public getModuleFromApi() {
+        console.log(`getModule called`);
+
+        return new Promise(function (resolve, reject) {
+            console.log("Making request..")
+            request(url, { json: true }, (err, res, body) => {
+                if (err) { reject(console.log(err)); }
+                console.log("Got request! Logging body..")
+                console.log(body);
+                let response = body;
+
+                args = response["root"]["inputs"];
+                console.log(args)
+
+                resolve(_.map(args, o => {
+                    let c = new CompletionItem(`${o.name} (${module})`, CompletionItemKind.Property);
+                    c.kind = CompletionItemKind.Variable;
+                    let def = "";
+                    if (o.required) {
+                        def = "Required - "
+                    } else {
+                        def = `Optional (Default: ${o.default}) - `
+                    }
+                    c.detail = def + o.description;
+                    c.insertText = o.name;
+                    return c;
+                }));
+
+            });
+        });
     }
 }
