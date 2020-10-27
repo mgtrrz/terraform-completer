628fe6c4fa1d97abef2f390a38814133db32b2ea
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-10-26 22:25:02 -0500

    Grab token from terraformrc file

--- src/TerraformApi.ts
+++ src/TerraformApi.ts
@@ -14,22 +14,26 @@ var urls = require("../../aws-urls.json");
 console.log(urls)
 
 const HOME_DIR = require('os').homedir();
+const TOKEN_REGEX = /^\s*token = "([a-zA-Z0-9.]+)"$/
 
 export class TerraformApi {
     public apiTokenExists (): boolean {
         console.log(`apiTokenExists called`);
         console.log(workspace.getConfiguration('terraform').get('terraformrc_file_path'));
 
-        const tfrcFile = HOME_DIR +'/.terraformrc'
-
+        const tfrcFile = HOME_DIR + '/.terraformrc'
         console.log(tfrcFile)
-        if (fs.existsSync(tfrcFile)) {
-            // File exists in path
-            console.log("File exists")
-            let res = fs.readFileSync(tfrcFile,'utf8');
-            console.log(res)
-        }
 
+        if (fs.existsSync(tfrcFile)) {
+            let tfrcContents = fs.readFileSync(tfrcFile, 'utf8');
+            for (let line of tfrcContents.split("\n")) {
+                if (TOKEN_REGEX.test(line)) {
+                    console.log("Successfully found token using regex: ");
+                    const token = TOKEN_REGEX.exec(line)[1];
+                    console.log(token)
+                }
+            }
+        }
 
         return false;
     }
